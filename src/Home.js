10d7925d255c2fd2d4f4b0f0c780ccdb8d3efe1c
tsion bantegize <166711429+tsionbantegize16@users.noreply.github.com>
2025-04-08 19:42:51 +0300

    to display a list of blog posts

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,34 @@
 import { useState } from "react";
- 
- const Home = () => {
-   // let name = 'mario';
-   const [name, setName] = useState('mario');
-   const [age, setAge] = useState(25);
- 
-   const handleClick = () => {
-     // name = 'luigi';
-     setName('luigi');
-     setAge(30);
-   }
-   const [blogs, setBlogs] = useState([
-     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-   ])
- 
-   return (
-     <div className="home">
-       <h2>Homepage</h2>
-       <p>{ name } is { age } years old</p>
-       <button onClick={handleClick}>Click me</button>
-       {blogs.map(blog => (
-         <div className="blog-preview" key={blog.id} >
-           <h2>{ blog.title }</h2>
-           <p>Written by { blog.author }</p>
-         </div>
-       ))}
-     </div>
-   );
- }
+import BlogList from "./BlogList"; // Assuming BlogList is in a separate file
+
+const Home = () => {
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName('luigi');
+    setAge(30);
+  }
+
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <p>{ name } is { age } years old</p>
+      <button onClick={handleClick}>Click me</button>
+      
+      {/* BlogList displaying all blogs */}
+      <BlogList blogs={blogs} title="All Blogs" />
+      
+      {/* BlogList displaying only blogs authored by Mario */}
+      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
+    </div>
+  );
+}
+
 export default Home;
