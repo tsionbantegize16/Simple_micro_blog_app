3b3064c29660a61b01f205274522d0cc7d950614
tsion bantegize <166711429+tsionbantegize16@users.noreply.github.com>
2025-04-08 21:02:23 +0300

    defines a Home component in a React application

--- src/Home.js
+++ src/Home.js
@@ -2,31 +2,21 @@ import { useState } from "react";
 import BlogList from "./BlogList"; // Assuming BlogList is in a separate file
 
 const Home = () => {
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
-
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 }
   ]);
+
+  // Function to handle blog deletion
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
-      
-      {/* BlogList displaying all blogs */}
-      <BlogList blogs={blogs} title="All Blogs" />
-      
-      {/* BlogList displaying only blogs authored by Mario */}
-      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
+      {/* Displaying all blogs */}
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 }
