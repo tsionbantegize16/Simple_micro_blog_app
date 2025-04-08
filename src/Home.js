5c909702eda031d9cfdd4e4345bf62fbde8fdad7
tsion bantegize <166711429+tsionbantegize16@users.noreply.github.com>
2025-04-08 21:06:22 +0300

    some minor formatting adjustments to enhance clarity and consistency

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,30 @@
-import { useState } from "react";
-import BlogList from "./BlogList"; // Assuming BlogList is in a separate file
+import { useState, useEffect } from "react";  // Ensure useState and useEffect are imported correctly
+import BlogList from "./BlogList"; // Import BlogList component
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
+  const [blogs, setBlogs] = useState([ // Initializing state with an array of blogs
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 }
   ]);
 
   // Function to handle blog deletion
   const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+    const newBlogs = blogs.filter(blog => blog.id !== id); // Filter out the blog with the specified id
+    setBlogs(newBlogs); // Update the state with the new array
+  };
+
+  // Using useEffect to log the blogs whenever they change
+  useEffect(() => {
+    console.log('useEffect ran'); // This will run after the initial render and whenever the blogs array changes
+    console.log(blogs); // Logs the current state of the blogs array
+  }, [blogs]); // The effect will only run when 'blogs' changes
 
   return (
     <div className="home">
-      {/* Displaying all blogs */}
+      {/* Displaying all blogs with BlogList component */}
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
-}
+};
 
 export default Home;
